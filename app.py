4d9aadd28eef928112cf8c58bb2feb8cde4ea0a1
phawitb <96680592+phawitb@users.noreply.github.com>
2024-03-25 10:52:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+#----------------------Hide Streamlit footer----------------------------
+hide_st_style = “”"
 
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+
+“”"
+st.markdown(hide_st_style, unsafe_allow_html=True)
+#--------------------------------------------------------------------
 
 
 
