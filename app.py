7831447b4579a7960411527f931fa861b1a58ca4
phawitb <96680592+phawitb@users.noreply.github.com>
2024-03-25 10:50:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 st.set_page_config(layout="wide")
 
