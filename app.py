43e230dd5b8ee35ea18bd8530ebf97a432dbb39d
phawitb <96680592+phawitb@users.noreply.github.com>
2024-03-25 10:53:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 #----------------------Hide Streamlit footer----------------------------
-hide_st_style = “”"
+hide_st_style = """
 
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
 
-“”"
+"""
 st.markdown(hide_st_style, unsafe_allow_html=True)
 #--------------------------------------------------------------------
 
