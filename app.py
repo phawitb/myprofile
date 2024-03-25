23d88abbd0204ad34390b03d69da20841e89a124
phawitb <96680592+phawitb@users.noreply.github.com>
2024-03-25 10:54:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.set_page_config(layout="wide")
+
 #----------------------Hide Streamlit footer----------------------------
 hide_st_style = """
-
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
-
 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 #--------------------------------------------------------------------
-
+st.set_page_config(layout="wide")
 
 
 COL1 = st.columns([1, 2],gap='large')
